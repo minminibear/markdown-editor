1f5961fff844a7435cf2bdf648853e26a387f325
minminibear <tell020114@gmail.com>
2022-04-02 06:49:05 +0900

    TypeScriptの導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts'],
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
@@ -9,8 +21,13 @@ module.exports = {
     }
 }
 
+// moduleのrulesセクションでwebpackに対してビルド時に追加で行う処理を記述。
+// test：.tsで終わるファイルに対して
+// use: ts-loaderを実行する
+// exclude:node-modules配下のファイル(＝外部ライブラリ)は除外
+
 // entry:最初に読み込むファイルを指定。 
 // 指定されたファイルから別のファイルを読み込む処理が書かれていると、webpack はそれらのファイルも自動的に読み込んで、最終的に1つのファイルとして出力。
 
 // output:出力するファイルの設定
-// webpack.config.js の置いてあるディレクトリにある distに対して、index.js で出力。 また、変換する際は JavaScript 内に書かれている相対パスのリソースへ自動的に dist/ を追加。 
+// webpack.config.js の置いてあるディレクトリにある distに対して、index.js で出力。 また、変換する際は JavaScript 内に書かれている相対パスのリソースへ自動的に dist/ を追加。
