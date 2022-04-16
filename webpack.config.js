687816f5fdd289d39c18066b5d5dac3f959e873e
minminibear <tell020114@gmail.com>
2022-04-16 15:23:38 +0900

    ウェブアプリマニフェストの実装

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[contenthash]index.js',
+        filename: 'index.js',
         publicPath: '/dist/',
     },
     devServer: {
